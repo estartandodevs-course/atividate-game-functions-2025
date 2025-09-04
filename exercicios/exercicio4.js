b22af4a4d7b95731bf96eacec6704c0595aa871e
Bruno Fabbio <brunobiodev@gmail.com>
2025-09-03 21:48:00 -0300

    feat: finalizei o exercicio 4

---
+++ exercicios/exercicio4.js
@@ -0,0 +1,19 @@
+
+const dados = require("../data/games_data");
+
+function jogoDisponivelParaPC(titulo) {
+    const jogo = dados.jogos.find((jogo) => jogo.titulo === titulo);
+    if (jogo) {
+        return jogo.plataformas.includes("PC");
+    }
+    return false;
+}
+
+const titulo = "Final Fantasy VII Remake";
+const disponivelParaPC = jogoDisponivelParaPC(titulo);
+
+if (disponivelParaPC) {
+    console.log(`O jogo "${titulo}" está disponível para PC.`);
+} else {
+    console.log(`O jogo "${titulo}" não está disponível para PC.`);
+}
